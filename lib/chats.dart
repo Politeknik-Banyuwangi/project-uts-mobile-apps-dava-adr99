d865d22abfa1bb4c4c488be0f17110309ecaf3b1
dava-adr99 <davahanyanama@gmail.com>
2022-05-31 11:59:46 +0700

    membuat card chat secara manual, belum ada Listbuilder

--- lib/chats.dart
+++ lib/chats.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:ui_whatsapp/widgets/CardChats.dart';
 
 class Chats extends StatelessWidget {
   const Chats({Key? key}) : super(key: key);
@@ -13,7 +14,10 @@ class Chats extends StatelessWidget {
           return true;
         },
         child: ListView(
-          children: [],
+          children: [
+            CardsChat(),
+            CardsChat(),
+          ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
--- lib/widgets/CardChats.dart
+++ lib/widgets/CardChats.dart
@@ -7,8 +7,27 @@ class CardsChat extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListTile(
       leading: CircleAvatar(
+        backgroundImage: NetworkImage(
+            "https://i0.wp.com/www.cirebonkota.go.id/wp-content/uploads/2018/05/jokowi.jpg"),
         radius: 30.0,
       ),
+      title: Text(
+        'Anda',
+        style: TextStyle(fontWeight: FontWeight.bold),
+      ),
+      subtitle: Row(
+        children: [
+          Icon(
+            Icons.done_all_rounded,
+            color: Colors.blue,
+          ),
+          SizedBox(
+            width: 3.0,
+          ),
+          Text('Tugasmu urung mari dap'),
+        ],
+      ),
+      trailing: Text('18.00'),
     );
   }
 }
